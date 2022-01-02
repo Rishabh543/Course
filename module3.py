17dde004adcb937a5713e084542b4d25ecfef265
Rishabh543 <shekharrishabh3@gmail.com>
2022-01-02 10:39:23 +0530

    This is module 3

---
+++ module3.py
@@ -0,0 +1,4 @@
+def learned_course():
+    print ("My name is Rishabh")
+    print("The module name is course3")
+    print("Understanding the prolem and implementing a solution which is space and time efficient by structuring it into sub problems.")
